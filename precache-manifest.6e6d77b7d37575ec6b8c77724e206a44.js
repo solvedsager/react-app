788f2c4cda0895d287c2ae66a190dcc81b8c8a36
Sager <sager.brown@gmail.com>
2019-11-11 18:31:05 -0600

    Updates

---
+++ precache-manifest.6e6d77b7d37575ec6b8c77724e206a44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "bf2cdfa9b34ddc2270ff",
+    "url": "/react-portfolio/static/js/main.bf2cdfa9.chunk.js"
+  },
+  {
+    "revision": "9af1334f17c05fb503b6",
+    "url": "/react-portfolio/static/js/1.9af1334f.chunk.js"
+  },
+  {
+    "revision": "bf2cdfa9b34ddc2270ff",
+    "url": "/react-portfolio/static/css/main.08cc6209.chunk.css"
+  },
+  {
+    "revision": "c171c421d23938c75aa24ff1d4ea2328",
+    "url": "/react-portfolio/index.html"
+  }
+];
